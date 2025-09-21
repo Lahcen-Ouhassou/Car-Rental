f7b3996422bfa59043935f5f79631ce858f54c67
Lahcen-Ouhassou <Lahcenouhassou18@gmail.com>
2025-09-21 16:22:56 +0100

    :recycle: refactor: mise a jour configuration Google OAuth avec phpdotenv

--- includes/google-login.php
+++ includes/google-login.php
@@ -1,14 +1,19 @@
 <?php
 require_once 'vendor/autoload.php';
-
 session_start();
 
+// Charger le .env depuis le même dossier
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); // __DIR__ = includes/
+$dotenv->load();
+
+// Utilisation des variables
 $client = new Google_Client();
-$client->setClientId('410622163909-9v2upvbgtmf9784umajlrrpvq7mmsora.apps.googleusercontent.com');
-$client->setClientSecret('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');
-$client->setRedirectUri('http://localhost/carrental/includes/google-callback.php');
+$client->setClientId($_ENV['GOOGLE_CLIENT_ID']);
+$client->setClientSecret($_ENV['GOOGLE_CLIENT_SECRET']);
+$client->setRedirectUri($_ENV['GOOGLE_REDIRECT_URI']);
 $client->addScope('email');
 $client->addScope('profile');
 
 $authUrl = $client->createAuthUrl();
 header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
+
